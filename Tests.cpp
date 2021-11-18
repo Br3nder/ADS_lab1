69bafbac0a898847287cba7a60b0c0c17e48cdca
Br3nder <92253450+Br3nder@users.noreply.github.com>
2021-11-18 21:43:40 +0300

    I DID IT

--- Tests.cpp
+++ Tests.cpp
@@ -13,45 +13,45 @@ namespace Tests
 		List* list = new List(new Element(1, "Vadim"));
 		TEST_METHOD(TestPushBack)
 		{
-			list->pushBack(new Element(2, "Alexandra"));
+			list->pushBack(2, "Alexandra");
 			Assert::IsTrue(!list->isEmpty() && list->getSize() == 2);
 		}
 		TEST_METHOD(TestPushFront)
 		{
-			list->pushBack(new Element(2, "Alexandra"));
+			list->pushBack(2, "Alexandra");
 			Assert::IsTrue(!list->isEmpty() && list->getSize() == 2);
 		}
 		TEST_METHOD(TestPopBack)
 		{
-			list->pushBack(new Element(2, "Alexandra"));
+			list->pushBack(2, "Alexandra");
 			list->popBack();
 			Assert::IsTrue(!list->isEmpty() && list->getSize() == 1);
 		}
 		TEST_METHOD(TestPopFront)
 		{
-			list->pushBack(new Element(2, "Alexandra"));
+			list->pushBack(2, "Alexandra");
 			list->popFront();
 			Assert::IsTrue(!list->isEmpty() && list->getSize() == 1);
 		}
 		TEST_METHOD(TestInsert)
 		{
-			list->pushBack(new Element(2, "Alexandra"));
+			list->pushBack(2, "Alexandra");
 			List* list2 = new List(new Element(3, "Ingvar"));
-			list2->pushBack(new Element(4, "Valgard"));
+			list2->pushBack(4, "Valgard");
 			list->insert(list2, 1);
 			Assert::IsTrue(!list->isEmpty() && list->getSize() == 4);
 		}
 		TEST_METHOD(TestRemove)
 		{
-			list->pushBack(new Element(2, "Alexandra"));
-			list->pushBack(new Element(3, "Ingvar"));
-			list->pushBack(new Element(4, "Valgard"));
+			list->pushBack(2, "Alexandra");
+			list->pushBack(3, "Ingvar");
+			list->pushBack(4, "Valgard");
 			list->remove(3);
 			Assert::IsTrue(!list->isEmpty() && list->getSize() == 3);
 		}
 		TEST_METHOD(TestGetSize)
 		{
-			list->pushBack(new Element(2, "Alexandra"));
+			list->pushBack(2, "Alexandra");
 			Assert::IsTrue(!list->isEmpty() && list->getSize() == 2);
 		}
 		TEST_METHOD(TestClear)
@@ -61,9 +61,8 @@ namespace Tests
 		}
 		TEST_METHOD(TestSet)
 		{
-			Element* newMan = new Element(1, "Alexandra");
-			list->set(newMan, 1);
-			Assert::IsTrue(list->at(1) == newMan);
+			list->set(1, "Alexandra", 1);
+			Assert::IsTrue(list->at(1)->getName() == "Alexandra");
 		}
 		TEST_METHOD(TestIsEmpty)
 		{
@@ -72,11 +71,28 @@ namespace Tests
 		}
 		TEST_METHOD(TestAt)
 		{
-			Element* newMan1 = new Element(2, "Alexandra");
-			Element* newMan2 = new Element(3, "Alex");
-			list->pushBack(newMan1);
-			list->pushBack(newMan2);
-			Assert::IsTrue(list->at(2) == newMan1);
+			list->pushBack(2, "Alexandra");
+			list->pushBack(3, "Alex");
+			Assert::IsTrue(list->at(2)->getName() == "Alex");
+		}
+		TEST_METHOD(oneManPopBack)
+		{
+			List* list = new List();
+			list->pushBack(0,"Alexandra");
+			list->popBack();
+			Assert::IsTrue(list->getElement()->getName() == "Alexandra");
+		}
+		TEST_METHOD(emptyPushBack)
+		{
+			List* list = new List();
+			list->pushBack(0, "Alexandra");
+			Assert::IsTrue(list->getElement()->getName() == "Alexandra");
+		}
+		TEST_METHOD(emptyPushFront)
+		{
+			List* list = new List();
+			list->pushFront(0, "Alexandra");
+			Assert::IsTrue(list->getElement()->getName() == "Alexandra");
 		}
 	};
 }
